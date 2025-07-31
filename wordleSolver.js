47743a79aa1518a20c58c58f1f4a2d06726599dc
Warrick199 <pectin.sixths_3s@icloud.com>
2025-07-31 11:48:05 -0500

    Create wordleSolver.js

---
+++ wordleSolver.js
@@ -0,0 +1,19 @@
+// wordleSolver.js
+
+const solutions = [ /* … your column-A words … */ ];
+const validWords = [ /* … your column-B words … */ ];
+
+function applyFilter(possible, feedback) {
+  // convert your VBA ApplyFilter here…
+  return possible;
+}
+function scoreAndSort(words) {
+  // tally frequencies, score, sort descending…
+  return words;
+}
+
+document.getElementById('run').addEventListener('click', () => {
+  // 1) read feedback from inputs…
+  // 2) filter / score / sort…
+  // 3) update #next-guess and #top5 in the DOM
+});
