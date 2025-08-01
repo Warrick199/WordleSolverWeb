7b0e6bf9212470930befcab5e0fd1024c621b28d
Warrick199 <pectin.sixths_3s@icloud.com>
2025-08-01 14:02:17 -0500

    Update LetterRow.jsx

--- src/components/LetterRow.jsx
+++ src/components/LetterRow.jsx
@@ -3,9 +3,12 @@ import LetterCell from './LetterCell'
 export default function LetterRow({ letters, statuses }) {
   return (
     <div className="flex justify-center my-2">
-      {letters.map((ltr, i) => (
+      {letters.map
+        ? letter.map((ltr, i) => (
         <LetterCell key={i} letter={ltr} status={statuses[i] || 'empty'} />
-      ))}
+      ))
+      : null
+      }
     </div>
   )
 }
